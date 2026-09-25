cd76728437bcb22ef7229ef9c59b3c2ba5cc1a05
zuleta24 <sebastianzuleta3@gmail.com>
2026-09-24 23:18:33 -0500

    Error color

--- visor-geografico/js/vias.js
+++ visor-geografico/js/vias.js
@@ -86,7 +86,7 @@ async function initViasLayer(map) {
   viasLayer = L.geoJSON(
     { type: 'FeatureCollection', features },
     {
-      style: { color: 'rgb(235, 74, 74)', weight: 2, opacity: 0.8 },
+      style: { color: 'rgb(184, 149, 148)', weight: 2, opacity: 0.8 },
       onEachFeature: (feature, layer) => layer.bindPopup(popupVia(feature.properties)),
     }
   );
